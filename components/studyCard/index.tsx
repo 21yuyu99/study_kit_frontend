135415d31c3cd79c5c1caecd5c0dc5eb145fbd84
JSeokjun <sjjjang00@gmail.com>
2023-01-18 21:33:46 +0900

    progressBar 수정

--- components/studyCard/index.tsx
+++ components/studyCard/index.tsx
@@ -5,14 +5,14 @@ const StudyCard = ()=>{
   return(
     <div className={styles.studyCard}>
       <div className={styles.cardType}>언어</div>
-      <div className={styles.cardTag}>#문제풀이 #실습</div>
-      <div className={styles.star}><AiFillStar size={25}/></div>
+      <div className={styles.cardTag}>#문제풀이</div>
+      <div className={styles.star}><AiFillStar size={27}/></div>
       <div className={styles.cardContent}>
         <div className={styles.cardTitle}>
           <h3>오픽 자격증 따기</h3>
         </div>
         <p>12월 18일~ (27일째 스터디 중)</p>
-        <progress value="80" max="100"></progress>
+        <div className={styles.progressBar}></div>
       </div>
     </div>
   )
