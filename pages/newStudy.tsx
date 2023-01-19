482de45e5d9e1cb5e2fb9fbdf599e6927c96d594
JSeokjun <sjjjang00@gmail.com>
2023-01-19 21:32:32 +0900

    새스터디만들기버튼 추가

--- pages/newStudy.tsx
+++ pages/newStudy.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/newStudy.module.scss';
 import { AiOutlineBell, AiOutlineSearch } from 'react-icons/ai';
+import { BsPlusLg } from 'react-icons/Bs';
 import StudyTypeBar from '@/components/studyTypeBar';
 import StudyCard from '@/components/studyCard';
 import BottomNavigation from '@/components/bottomNavigation';
@@ -9,7 +10,7 @@ export default function newStudy(){
   <nav className={styles.top}>
       <div className={styles.topLeftContainer}></div>
       <div className={styles.topTitle}>새 스터디</div>
-      <div className={styles.bell}><AiOutlineBell></AiOutlineBell></div>
+      <div className={styles.bell}><AiOutlineBell/></div>
   </nav>
   <main className={styles.main}>
     <div className={styles.banner}>
@@ -23,13 +24,21 @@ export default function newStudy(){
     </div>
     <form action="" method="GET" className={styles.searchBox}>
       <input type="text" placeholder='스터디주제, 이름 검색'></input>
-      <button type="submit" className={styles.searchButton}><AiOutlineSearch size="20"></AiOutlineSearch></button>
+      <button type="submit" className={styles.searchButton}><AiOutlineSearch size="20"/></button>
     </form>
     <StudyTypeBar/>
     <StudyCard/>
     <StudyCard/>
-    <StudyCard/>
-    <StudyCard/>
+    <div className={styles.createStudy}>
+      <div className={styles.createStudyButton}><BsPlusLg size="10"/></div>
+      <div className={styles.createStudyText}>
+        <div className={styles.createStudyTitle}>
+          <h3>새 스터디 만들기</h3>
+        </div>
+        <p>원하는 스터디가 없다면? 스터디장이 될 차례!</p>
+      </div>
+    </div>
+    <div className={styles.noSearchResult}>더이상 검색결과가 없습니다</div>
   </main>
   <BottomNavigation/>
   </>
