cbe027efd9d35b883bc9180a896044526a3988b4
21yuyu99 <yujeong0720@gmail.com>
2023-02-11 22:18:59 +0900

    pagination 퍼블리싱

--- pages/studyBoard/index.tsx
+++ pages/studyBoard/index.tsx
@@ -5,7 +5,30 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { PreviewBoard } from '@/components/studyBoard';
 export default function StudyBoard(){
-
+  interface PageNumberType {
+    num : number,
+    point : boolean
+  }
+  const [pageNumbers,setPageNumbers] = useState<PageNumberType[]>([
+    {
+      num : 1,
+      point : true
+    },{
+      num: 2,
+      point: false
+    },{
+      num :3,
+      point: false
+    },
+    {
+      num:4,
+      point: false
+    },
+    {
+      num:5,
+      point: false
+    }
+  ]);
  return(
   <>
   <TopNavigation title={"토익 뽀개기"} backSpace={true} rightIcon={"bell"} backgroundStyle={2}></TopNavigation>
@@ -22,6 +45,24 @@ export default function StudyBoard(){
     <PreviewBoard title="p.135 질문 있습니다!" isChief={false} date="2023.01.23" views={50} comment={23} nickName="chlwldP"/>
     <PreviewBoard title="p.135 질문 있습니다!" isChief={false} date="2023.01.23" views={50} comment={23} nickName="chlwldP"/>
     <PreviewBoard title="p.135 질문 있습니다!" isChief={false} date="2023.01.23" views={50} comment={23} nickName="chlwldP"/>
+    <div className={styles.pagination}>
+      <div className={styles.back}>&#60;</div>
+      <div className={styles.pageNumberWrapper}>
+        {
+          pageNumbers.map((page)=>{
+            return(
+              <div className={page.point===true?styles.pagePoint:styles.pageNotPoint} key={page.num} onClick={()=>setPageNumbers(
+                  pageNumbers.map(
+                    x=>
+                    x.num===page.num?{...x,point:true}:{...x,point:false}
+                )
+              )}>{page.num}</div>
+            )
+          })
+        }
+      </div>
+      <div className={styles.next}>&#62;</div>
+    </div>    
   </main>
   </>
  )
