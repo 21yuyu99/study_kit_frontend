8754509030b5afac7c40b758039ba97d5fc639f9
JSeokjun <sjjjang00@gmail.com>
2023-02-12 20:19:04 +0900

    가입신청관리페이지 퍼블리싱

--- pages/studyMember/management.tsx
+++ pages/studyMember/management.tsx
@@ -1,7 +1,7 @@
 import styles from '@/styles/studyMember/management.module.scss'
 import TopNavigation from '@/components/topNavigation';
 import PlusMan from '@/public/img/plusMan.svg'
-import { Member } from '@/components/member';
+import { ApplyMember } from '@/components/member/apply';
 import { useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -38,6 +38,21 @@ export default function StudyMember(){
         <div className={toggle===true?`${styles.toggleButton_on}`:`${styles.toggleButton_off}`}></div>
       </div>
     </div>
+    <ApplyMember nickName={'가나다'} level={4} time={1}
+      intro={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      q1={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      q2={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      ischeck={false}/>
+    <ApplyMember nickName={'가나다'} level={4} time={3} 
+      intro={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      q1={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      q2={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      ischeck={true}/>
+    <ApplyMember nickName={'가나다'} level={4} time={3} 
+      intro={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      q1={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      q2={'안녕하세요. 영어 공부하려고 가입한 홍길순이에요.  혼자서는 도저히 공부가 안되네요. 받아주시면 감사하겠습니다 ㅠ'} 
+      ischeck={false}/>
   </main>
   </>
   )
