be25798b299b437212d104dcb2bfec1c9b8cf599
JSeokjun <sjjjang00@gmail.com>
2023-02-25 22:21:54 +0900

    commentCard 상태관리

--- components/commentCard/index.tsx
+++ components/commentCard/index.tsx
@@ -1,19 +1,37 @@
 import styles from "./index.module.scss";
 import { FaCircle } from "react-icons/fa";
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
+import { useState } from "react";
 interface Props {
   nickName: string;
+  isHead : boolean;
   content : string;
   date : string;
-  reply : number;
+  nestedCommentNum : number;
+  nestedComment : {
+    nickName: string;
+    isHead : boolean;
+    content : string;
+    date : string;
+ };
 }
 
-const CommentCard = ({nickName,content,date,reply} : Props)=>{
-
+const CommentCard = ({nickName,content,date,nestedComment,isHead,nestedCommentNum} : Props)=>{
+  const [toggle,setToggle] = useState(false);
+  const onClickToggle = ()=>{
+    if(toggle === true){
+      setToggle(false);
+    }
+    else{
+      setToggle(true);
+    }
+  }
   return(
     <div className={styles.main}>
       <div className={styles.topContainer}>
-        <div className={styles.photo}><FaCircle size={38}/></div>
+        <div className={isHead===true?styles.photo_yellow:styles.photo_gray}><FaCircle size={38}/></div>
         <div className={styles.nickName}>{nickName}</div>
+        {isHead===true&&<div className={styles.head}>스터디장</div>}
       </div>
       <div className={styles.midContainer}>
         <div className={styles.content}>{content}</div>
@@ -21,9 +39,25 @@ const CommentCard = ({nickName,content,date,reply} : Props)=>{
       <div className={styles.bottomContainer}>
         <div className={styles.date}>{date}</div>
         <div className={styles.reply}>답글쓰기</div>
-        <div className={styles.reply}>답글{reply}</div>
+        <div className={styles.reply}>답글{nestedCommentNum}</div>
         <div className={styles.remove}>삭제하기</div>
       </div>
+      {nestedCommentNum!==0&&toggle===false&&<div className={styles.toggle} onClick={()=>onClickToggle()}><MdOutlineKeyboardArrowUp/></div>}
+      {nestedCommentNum!==0&&toggle===true&&
+      <><div className={styles.topContainer}>
+          <div className={nestedComment.isHead===true?styles.photo_yellow:styles.photo_gray}><FaCircle size={38}/></div>
+          <div className={styles.nickName}>{nestedComment.nickName}</div>
+          {nestedComment.isHead===true&&<div className={styles.head}>스터디장</div>}
+        </div>
+        <div className={styles.midContainer}>
+          <div className={styles.content}>{nestedComment.content}</div>
+        </div>
+        <div className={styles.bottomContainer}>
+          <div className={styles.date}>{nestedComment.date}</div>
+          <div className={styles.reply}>답글쓰기</div>
+          <div className={styles.remove}>삭제하기</div>
+        </div>
+        {toggle===true&&<div className={styles.toggle} onClick={()=>onClickToggle()}><MdOutlineKeyboardArrowDown/></div>}</>}
     </div>
   )
 }
--- pages/studyBoard/showSingle.tsx
+++ pages/studyBoard/showSingle.tsx
@@ -2,11 +2,43 @@ import TopNavigation from "@/components/topNavigation";
 import CommentCard from "@/components/commentCard";
 import styles from "@/styles/studyBoard/showSingle.module.scss";
 const ShowSingle = ()=>{
+  const comment = [
+    {
+      id: 1,
+      nickname: '윤다인',
+      isHead: false,
+      content: "10주차 숙제 피드백 불참하여 벌금 제출했습니다.",
+      date: "2023.02.23",
+      nestedCommentNum: 1,
+      nestedComment: [
+        {
+          id: 1,
+          nickName: "강상우",
+          isHead : true,
+          content : "확인하였습니다.",
+          date : "2023.02.23",
+         },
+      ]
+    }, {
+      id: 2,
+      nickname: '윤다인',
+      isHead: false,
+      content: "10주차 숙제 피드백 불참하여 벌금 제출했습니다.",
+      date: "2023.02.23",
+      nestedCommentNum: 0,
+    },
+  ]
   return(
     <>
     <TopNavigation title={"토익 뽀개기"} backSpace={true} rightIcon={"bell"} backgroundStyle={2}></TopNavigation>
     <main className={styles.main}>
-      <CommentCard nickName={"장석준"} content={"10주차 숙제 피드백 불참하여 벌금 제출했습니다."} date={"2023.02.23"} reply={1}/>
+    {comment.map(x => <CommentCard nickName={x.nickname} isHead={x.isHead} content={x.content} date={x.date} nestedCommentNum={x.nestedCommentNum} 
+    nestedComment={{
+      nickName: "강상우",
+      isHead: true,
+      content: "확인하였습니다.",
+      date: "2023.02.23"
+    }}/>)}
     </main>
     </>
   )
